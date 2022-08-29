bbfe17e51b4069553a4b2d6c5364a10fc2907562
Flutter Developer <fdeveloper786@gmail.com>
2022-08-29 21:58:09 +0530

    Initial Commit

---
+++ Shared/iOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSProjectApp.swift
+//  Shared
+//
+//  Created by Mohammad Jawed Ahmad Ansari on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
